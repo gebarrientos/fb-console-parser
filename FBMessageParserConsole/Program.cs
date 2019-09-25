dce3c3bfbeb72a4951c1319efc8fdec92b5a6c75
gebarrientos <gebarrientos@gmail.com>
2019-09-24 20:10:35 -0500

    adding new files
    adding initial base files

---
+++ FBMessageParserConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace FBMessageParserConsole
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            string file = "C:\\Users\\WeebMachine\\source\\repos\\FBMessageParserConsole\\FBMessageParserConsole\\message_1.json";
+            RootObject firstPerson = new RootObject();
+            Console.WriteLine("Hello World!");
+            firstPerson = RootObject.Deserializer(file);
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
